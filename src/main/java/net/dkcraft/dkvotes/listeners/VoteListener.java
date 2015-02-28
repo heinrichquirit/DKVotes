524a0fcf4ac79927b3cb46ef6ae1a537d313436c
heinrichquirit <heinrichquirit@gmail.com>
2015-02-28 13:58:51 +1100

    Added UUID fetcher class. Now using this class fetch method to retrieve offline player UUIDs.

--- src/main/java/net/dkcraft/dkvotes/listeners/VoteListener.java
+++ src/main/java/net/dkcraft/dkvotes/listeners/VoteListener.java
@@ -1,6 +1,10 @@
 package main.java.net.dkcraft.dkvotes.listeners;
 
+import java.util.logging.Level;
+
 import main.java.net.dkcraft.dkvotes.DKVotes;
+import main.java.net.dkcraft.dkvotes.utils.UUIDFetcher;
+import main.java.net.dkcraft.dkvotes.utils.Util;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -20,12 +24,29 @@ public class VoteListener implements Listener {
 	public VoteListener(DKVotes p) {
 		this.p = p;
 	}
-	
+
+	@SuppressWarnings("deprecation")
 	@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
 	public void onVote(VotifierEvent event) {
-		@SuppressWarnings("deprecation")
-		Player player = Bukkit.getPlayer(event.getVote().getUsername());
-		String pId = player.getUniqueId().toString();
+		String name = event.getVote().getUsername();
+		Player player = Bukkit.getPlayer(name);
+		String pId = "";
+		if (player != null) {
+			pId = player.getUniqueId().toString();
+		} else {
+			try {
+				// Need to use UUID fetcher to fetch offline UUID since
+				// Spigots/Bukkits offline fetch method returns the wrong 
+				// UUID or is the MD5 checksum of the name string.
+				// This UUID fetcher utilizes Mojangs UUID api, which
+				// is a reliable way of fetching an accurate uuid.
+				pId = UUIDFetcher.getUUIDOf(name).toString();
+			} catch (Exception e) {
+				// Print error so they notice
+				Util.log(Level.SEVERE, "UUIDFetcher could not retrieve UUID for " + name + ", using OfflinePlayer fetch method instead.");
+				pId = Bukkit.getOfflinePlayer(name).getUniqueId().toString();
+			}
+		}
 		if (!p.vote_handler.getConfig().contains(pId)) {
 			p.vote_handler.reload();
 			p.vote_handler.getConfig().set(pId, 1);
